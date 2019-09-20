13f497ad9c9503187dea2eade1039682e104aff2
Sujiiii <sujithsuji1098@gmail.com>
2019-09-21 04:04:38 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+
+var app = express();
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp6');
+const sqlite3 = require('sqlite3').verbose();
+let ts = Date.now();
+
+var message;
+
+let db = new sqlite3.Database('aerophilia.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+        if (err) {
+            console.error("error " + err.message);
+        }
+        console.log('Connected to the aerophilia database.');
+    });
+
+try {
+    db.run('CREATE TABLE IF NOT EXISTS data(timestamp,value)');
+} catch (e) {
+
+}
+
+server.on('message', (msg, rinfo) => {
+    console.log("hello boo");
+    message = msg;
+    db.run(`INSERT INTO data(timestamp,value) VALUES(?,?)`, [ts, msg], function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    });
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+app.set('port', 8080);
+server.bind(8082);
+
+//parsing data from body
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+//setup view engine as ejs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.render('index.html',{message:message,ts:ts});
+});
+
+app.listen(8080, function () { 
+    console.log("server is running at localhost:8080");
+})
